2e8e62d1a82bde81c503c0bbf64df9db4247eab0
yatneshpuranik <yatneshpuranik@gmail.com>
2025-09-09 01:35:49 +0530

    update

---
+++ backend/model/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+ const userSchema =  new mongoose.model({
+    name :
+    {
+        type : String,
+        required : true ,
+    } ,
+    email :
+    {
+        type : String ,
+        required : true ,
+        unique : true ,
+    },
+    password :
+    {
+        type : String ,
+        required : true ,
+    },
+    isAdmin :
+    {
+        type : Boolean,
+        required : true ,
+        default : false , 
+
+    }
+ } , {
+ timestamp : true , 
+ })
+
+
+ const User = mongoose.model("User" , userSchema);
+  export default User ;
